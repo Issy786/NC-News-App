c64cd52f59a8076cedbda0ae8cbf005af1f0e41d
Issy786 <issy_02@hotmail.com>
2022-08-02 12:07:49 +0100

    completed ncnews-4 with a 404 and a 400 error messages

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
 const express = require("express");
-const { getTopics } = require("./controllers/controllers");
+const { getTopics, getArticlesById } = require("./controllers/controllers");
 const app = express();
 
 app.use(express.json());
 
 app.get("/api/topics", getTopics);
 
+app.get("/api/articles/:article_id", getArticlesById);
+
 app.all("/*", (req, res) =>
   res.status(404).send({ msg: "Endpoint not found" })
 );
+
+app.use((err, req, res, next) => {
+  if (err.code === "22P02") {
+    res.status(400).send({ msg: "Bad request" });
+  } else next(err);
+});
+
+app.use((err, req, res, next) => {
+  res.status(err.status).send({ msg: err.msg });
+});
 
 module.exports = app;
--- models/models.js
+++ models/models.js
@@ -5,3 +5,15 @@ exports.selectTopics = () => {
     return rows;
   });
 };
+
+exports.selectArticlesById = (article_id) => {
+  return db
+    .query("SELECT * FROM articles WHERE article_id = $1;", [article_id])
+    .then(({ rows }) => {
+      if (rows.length < 1) {
+        return Promise.reject({ status: 404, msg: "Invalid article id" });
+      } else {
+        return rows[0];
+      }
+    });
+};
