342c462845309097a2a9cbfc4e626d2b5ea005cf
Issy786 <issy_02@hotmail.com>
2022-08-17 14:35:58 +0100

    updated listen.js

--- listen.js
+++ listen.js
@@ -2,4 +2,7 @@ const app = require("./app");
 
 const { PORT = 9090 } = process.env;
 
-app.listen(PORT);
+app.listen(PORT, (error) => {
+  if (error) throw error;
+  console.log(`listening on ${PORT}`);
+});
