565f893db7542e2e7071ce001648a9c9bd24c571
Issy786 <issy_02@hotmail.com>
2022-08-22 15:18:16 +0100

    allow cross origin resource sharing

--- app.js
+++ app.js
@@ -10,9 +10,13 @@ const {
   deleteCommentById,
   getApi,
 } = require("./controllers/controllers");
+const cors = require("cors");
+
 const app = express();
 
 app.use(express.json());
+
+app.use(cors());
 
 app.get("/api", getApi);
 
