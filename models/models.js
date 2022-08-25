0f9c16eb29697cdfe5d5d6910162b0c5042228cf
Issy786 <issy_02@hotmail.com>
2022-08-25 09:57:54 +0100

    updated comments order by created_at DESC

--- models/models.js
+++ models/models.js
@@ -97,9 +97,10 @@ exports.selectArticles = (
 
 exports.selectCommentsByArticleId = (article_id) => {
   return db
-    .query("SELECT * FROM comments WHERE comments.article_id = $1;", [
-      article_id,
-    ])
+    .query(
+      "SELECT * FROM comments WHERE comments.article_id = $1 ORDER BY comments.created_at DESC;",
+      [article_id]
+    )
     .then(({ rows }) => {
       return rows;
     });
